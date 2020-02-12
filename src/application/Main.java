49a8339bd7e7176a72a5b02ee5d8f7b971312dba
JongHoJang <jcho5078@gmail.com>
2020-02-12 22:53:16 +0900

    JavaFx Husk

---
+++ src/application/Main.java
@@ -0,0 +1,32 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("Root.fxml"));
+			AnchorPane main = (AnchorPane) loader.load();
+			Scene scene = new Scene(main);
+				
+			primaryStage.setTitle("Java Fx");
+			primaryStage.setResizable(false);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
