12f8097a1c83075652ccf8e053ab4195cd1be851
JongHoJang <jcho5078@gmail.com>
2020-02-20 23:09:41 +0900

    SignUp Page move

--- src/application/MovePage.java
+++ src/application/MovePage.java
@@ -4,32 +4,49 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 public class MovePage implements Initializable {
-	@FXML private BorderPane bp;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
+		
 	}
+	
 	@FXML
-	private void movePage(MouseEvent event) {
-		loadPage();
+	private TextField txtUserName;
+	
+	@FXML
+	private TextField txtUserPw;
+	
+	@FXML
+	private void SignUp(ActionEvent event) throws Exception {
+		Stage primaryStage = new Stage();
+		Parent root = FXMLLoader.load(getClass().getResource("Root2.fxml"));
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("SignUp Page");
+		primaryStage.show();
 	}
 	
 	
 	public void loadPage(){
 		try {
 			Parent signup = FXMLLoader.load(getClass().getResource("Root2.fxml"));
-			bp.setCenter(signup);//아마 이게 문제인듯.
-			//페이지 이동 아직 실패.
+			Scene scene = new Scene(signup);
+			//Stage primaryStage = (Stage);
 			
 		}catch (Exception e) {
 			// TODO: handle exception
